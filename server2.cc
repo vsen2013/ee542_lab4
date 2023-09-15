b612f5a33480b7190c32ff5c6e04fe3ca0dbe97b
vsen2013 <52949518+vsen2013@users.noreply.github.com>
2023-09-14 18:19:18 -0700

    Update server2.cc

--- server2.cc
+++ server2.cc
@@ -7,80 +7,97 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <fcntl.h>
+#include <assert.h>
 
-#define PORT 3038
-#define MAXLINE 16388
+#include "util.h"
 
-std::unordered_map<uint32_t, bool> ack_map; // Map to track received packets
-
-void send_acknowledgment(int sock_fd, const struct sockaddr *clientaddr, socklen_t len) {
-    // Create an acknowledgment packet
-    char ack_buf[4] = "ACK";
-    sendto(sock_fd, ack_buf, sizeof(ack_buf), MSG_CONFIRM, clientaddr, len);
-}
-
-int main() {
-    int sock_fd;
-    struct sockaddr_in servaddr, clientaddr;
-
-    if ((sock_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
+class Server {
+public:
+  Server() = default;
+  Status Init() {
+    if((sock_fd_ = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+      return Status::IO_ERROR;
     }
 
-    memset(&servaddr, 0, sizeof(servaddr));
-    memset(&clientaddr, 0, sizeof(clientaddr));
+    memset(&servaddr_, 0, sizeof(servaddr_));
+    memset(&clientaddr_, 0, sizeof(clientaddr_));
 
-    servaddr.sin_family = AF_INET; // IPv4
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons(PORT);
+    servaddr_.sin_family    = AF_INET; // IPv4
+    servaddr_.sin_addr.s_addr = INADDR_ANY;
+    servaddr_.sin_port = htons(PORT);
 
-    if (bind(sock_fd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
+    if (bind(sock_fd_, (const struct sockaddr *)&servaddr_, sizeof(servaddr_)) < 0 )
+    {
+      return Status::IO_ERROR;
     }
 
-    // Create or open the file for writing
-    int fd = open("./result.txt", O_CREAT | O_RDWR);
+    return Status::Ok;
+  }
 
+  Status Run() {
     char buffer[MAXLINE];
-    socklen_t len = sizeof(clientaddr);
-    // Receive the file size from the client
-    int recvd_len = recvfrom(sock_fd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *)&clientaddr, &len);
+    socklen_t len = sizeof(clientaddr_);
+
+    // Get filename
+    int filename_len = recvfrom(sock_fd_, (char*) buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &clientaddr_, &len);
+    if (filename_len == -1) {
+      return Status::IO_ERROR;
+    }
+    buffer[filename_len] = '\0';
+    std::string filename(buffer);
+    filename = "./uploaded_" + filename;
+
+    // open/create file
+    int fd = open(filename.c_str(), O_CREAT | O_RDWR);
+    if(fd == -1) {
+      return Status::IO_ERROR;
+    }
+    memset(buffer, 0, MAXLINE);
+
+    // Get filesize
+    int recvd_len = recvfrom(sock_fd_, (char*) buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &clientaddr_, &len);
+    if(recvd_len == -1) return Status::IO_ERROR;
     buffer[recvd_len] = '\0';
     ssize_t filesize = stoi(std::string(buffer));
     std::cout << "file size is: " << filesize << std::endl;
     if (fallocate(fd, FALLOC_FL_KEEP_SIZE, 0, filesize) == -1) {
-        perror("fallocate failed");
-        exit(EXIT_FAILURE);
+      return Status::IO_ERROR;
     }
     memset(buffer, 0, MAXLINE);
 
     int cur = 0;
-    while (cur < filesize) {
-        int recvd_len = recvfrom(sock_fd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *)&clientaddr, &len);
-        if (recvd_len != -1) {
-            // Extract the block index from the packet
-            uint32_t index;
-            memcpy(&index, buffer, sizeof(uint32_t));
-            // Check if the packet has not been received before
-            if (ack_map.find(index) == ack_map.end()) {
-                // Write data to offset
-                int written_len = pwrite(fd, buffer + sizeof(uint32_t), recvd_len - sizeof(uint32_t), index * 16384);
-                if (written_len == -1) {
-                    perror("write data to disk failed");
-                    exit(EXIT_FAILURE);
-                }
-                cur += written_len;
-                // Mark the packet as received in the acknowledgment map
-                ack_map[index] = true;
-                std::cout << "\rcurrent size: " << cur << ", get block index: " << index << std::endl;
-            }
-            // Send acknowledgment for the received packet
-            send_acknowledgment(sock_fd, (struct sockaddr *)&clientaddr, len);
-        }
+    while(cur < filesize) {
+      int recvd_len = recvfrom(sock_fd_, (char*) buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &clientaddr_, &len);
+      if(recvd_len != -1) {
+        // decode to get block index
+        uint32_t index;
+        memcpy(&index, buffer, sizeof(uint32_t));
+        // send back ACK
+        int index_send_len = sendto(sock_fd_, (const char*)&index, sizeof(uint32_t), MSG_CONFIRM, (struct sockaddr *) &clientaddr_, sizeof(clientaddr_));
+        assert(index_send_len == sizeof(uint32_t));
+        // write data to offset
+        int written_len = pwrite(fd, buffer + sizeof(uint32_t), recvd_len - sizeof(uint32_t), index * 16384);
+        assert(written_len != -1);
+        cur += written_len;
+        std::cout << "\rcursize: " << cur << ", get block index: " << index << std::endl;
+      }
     }
-
     close(fd);
-}
+    return Status::Ok;
+  }
+private:
+  int sock_fd_, fd_;
+  struct sockaddr_in servaddr_, clientaddr_;
+};
 
+int main() {
+  Server server;
+  Status s = server.Init();
+  if(s != Status::Ok) {
+    exit(EXIT_FAILURE);
+  }
+  s = server.Run();
+  if(s != Status::Ok) {
+    exit(EXIT_FAILURE);
+  }
+}
